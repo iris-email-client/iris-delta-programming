a16075d18332de2d435bb4b2dd158371ac37c5b3
pedro <phtcosta@gmail.com>
2014-11-13 21:48:35 -0200

    updating some commands

--- src/test/java/br/unb/cic/iris/core/TestFolderDAO.java
+++ src/test/java/br/unb/cic/iris/core/TestFolderDAO.java
@@ -1,7 +1,12 @@
 package br.unb.cic.iris.core;
 
+import java.util.Date;
+import java.util.List;
+
 import irisdeltaj.simpleaddressbook.br.unb.cic.iris.core.exception.DBException;
+import irisdeltaj.simpleaddressbook.br.unb.cic.iris.core.model.EmailMessage;
 import irisdeltaj.simpleaddressbook.br.unb.cic.iris.core.model.IrisFolder;
+import irisdeltaj.simpleaddressbook.br.unb.cic.iris.persistence.sqlite3.EmailDAO;
 import irisdeltaj.simpleaddressbook.br.unb.cic.iris.persistence.sqlite3.FolderDAO;
 
 import org.junit.Assert;
@@ -15,7 +20,7 @@ public class TestFolderDAO {
 	@Before
 	public void setUp() throws Exception {
 		try {
-			dao = new FolderDAO();
+			dao = FolderDAO.instance();
 			
 			IrisFolder inbox = dao.findByName(IrisFolder.INBOX);
 			if(inbox == null){
@@ -52,6 +57,26 @@ public class TestFolderDAO {
 		catch(Exception e) {
 			e.printStackTrace();
 			Assert.fail("error while testing FolderDAO.saveOrUpdate()");
+		}
+	}
+	
+	@Test
+	public void testListMessages(){
+		try {
+			IrisFolder inbox = dao.findByName(IrisFolder.INBOX);
+			EmailDAO.instance().saveMessage(new EmailMessage("from-1", "to-1", "subject-1", "message-1",new Date(System.currentTimeMillis()), inbox));
+			EmailDAO.instance().saveMessage(new EmailMessage("from-1", "to-1", "subject-2", "message-2",new Date(), inbox));
+			EmailDAO.instance().saveMessage(new EmailMessage("from-2", "to-1", "subject-3", "message-3",new Date(), inbox));
+			EmailDAO.instance().saveMessage(new EmailMessage("from-2", "to-1", "subject-4", "message-4",new Date(), inbox));
+			
+			List<EmailMessage> list = EmailDAO.instance().listMessages(inbox.getId());
+			Assert.assertNotNull(list);
+			Assert.assertFalse(list.isEmpty());
+			for(EmailMessage msg: list){
+				System.out.println("MSG: "+msg);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
